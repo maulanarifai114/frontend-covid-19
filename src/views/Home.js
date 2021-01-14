bf51523496b074d399ae8fe8599546b3a951b0c1
maulanarifai114 <maulanarifai114@gmail.com>
2021-01-14 21:12:14 +0700

    try react

---
+++ src/views/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../assets/css/home.scoped.css'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>Halo home</h1>
+    </div>
+  )
+}
