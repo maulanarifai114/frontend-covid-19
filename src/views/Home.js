e91d135ed3386a098154146bf6654726cbf48f63
maulanarifai114 <maulanarifai114@gmail.com>
2021-01-16 00:04:32 +0700

    add line chart (integrated)

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import moment from 'moment'
+import { Line } from 'react-chartjs-2';
 import Title from '../components/Title'
 import Today from '../components/Today'
 import '../assets/css/home.css'
+import '../assets/css/mb30.css'
+
+const idLocale = require('moment/locale/id'); 
+moment.locale('id', idLocale)
 
 export default function Home() {
   let [cases, setCases] = useState(0)
@@ -13,10 +18,22 @@ export default function Home() {
   let [death, setDeath] = useState(0)
   let [plusDeath, setPlusDeath] = useState(0)
   let [date, setDate] = useState(null)
+  let [dataChartLine, setChartLine] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: 'Covid - 19 Indonesia',
+        data: [0,0,0],
+        backgroundColor: [
+          '#f0f358',
+          '#72ee62',
+          '#ee6262',
+        ],
+      }
+    ]
+  })
 
   const funcSetDate = (payload)=> {
-    const idLocale = require('moment/locale/id'); 
-    moment.locale('id', idLocale)
     const splitT = payload.split('T')
     const splitZ = splitT[1].split('.000Z')
     const date = `${splitT[0]} ${splitZ[0]}`
@@ -25,6 +42,50 @@ export default function Home() {
   const url = 'https://apicovid19indonesia-v2.vercel.app/api/indonesia'
 
   useEffect(() => {
+    axios.get(`${url}/harian`)
+    .then((res)=> {
+      const start = res.data.length-1-7
+      const end = res.data.length-1
+      const allData = res.data
+      let labels = []
+      let cases = []
+      let secure = []
+      let death = []
+      allData.forEach((e, index)=> {
+        const day = e.tanggal.split('T')
+        if (index > start && index <= end) {
+          labels.push(moment(day[0]).format('dddd'))
+          cases.push(e.positif_kumulatif)
+          secure.push(e.sembuh_kumulatif)
+          death.push(e.meninggal_kumulatif)
+        }
+      })
+      console.log('labels', labels);
+      setChartLine({
+        labels: labels,
+        datasets: [
+          {
+            label: 'Total Kasus',
+            data: cases,
+            fill: false,
+            borderColor: '#f0f358',
+          },
+          {
+            label: 'Sembuh',
+            data: secure,
+            fill: false,
+            borderColor: '#72ee62'
+            
+          },
+          {
+            label: 'Meninggal',
+            data: death,
+            fill: false,
+            borderColor: '#ee6262'
+          },
+        ]
+      })
+    })
     axios.get(`${url}/more`)
     .then((res)=> {
       const today = res.data
@@ -40,6 +101,7 @@ export default function Home() {
     .catch((err)=> {
       console.log(err.response);
     })
+    
   }, []);
 
   return (
@@ -53,6 +115,41 @@ export default function Home() {
         secure={secure} plusSecure={plusSecure}
         death={death} plusDeath={plusDeath}
       />
+      <hr/>
+      <h3 className="subtitle">1 Minggu Terakhir</h3>
+      <div className="mb30">
+        <Line  
+          data={dataChartLine}
+          height={300}
+          options={{
+            scales: {
+              yAxes: [{
+                ticks: {
+                  fontFamily: 'Quicksand',
+                  fontColor: 'white'
+                }
+              }],
+              xAxes: [{
+                ticks: {
+                  fontFamily: 'Quicksand',
+                  fontColor: 'white'
+                }
+              }]
+            },        
+            legend: {
+              labels: {
+                fontFamily: 'Quicksand',
+                fontColor: '#ffffff',
+                fontSize: 16
+              }
+              
+            },
+            responsive: true,
+            maintainAspectRatio: false 
+          }}
+        />
+      </div>
+      <hr/>
     </div>
   )
 }
